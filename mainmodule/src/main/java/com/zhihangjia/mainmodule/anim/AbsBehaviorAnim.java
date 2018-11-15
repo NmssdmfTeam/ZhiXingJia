3147c9da26871c4a932fd2fa10729f9ba1d28a5d
chenbin <wscb1030>
2018-11-15 10:43:01 +0800

    商品UI

---
+++ mainmodule/src/main/java/com/zhihangjia/mainmodule/anim/AbsBehaviorAnim.java
@@ -0,0 +1,43 @@
+package com.zhihangjia.mainmodule.anim;
+
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
+import android.view.animation.Interpolator;
+
+/**
+ * Desc : 抽象动画类，统一设置属性
+ * Author : lauzy
+ * Date : 2018/9/17
+ * Email : freedompaladin@gmail.com
+ */
+public abstract class AbsBehaviorAnim implements IBehaviorAnim {
+
+    private Interpolator mInterpolator = new LinearOutSlowInInterpolator();
+    private int mDuration = 400;
+
+    @Override
+    public Interpolator getInterpolator() {
+        return mInterpolator;
+    }
+
+    @Override
+    public void setInterpolator(Interpolator interpolator) {
+        mInterpolator = interpolator;
+    }
+
+    @Override
+    public int getDuration() {
+        return mDuration;
+    }
+
+    @Override
+    public void setDuration(int duration) {
+        mDuration = duration;
+    }
+
+    @Override
+    public abstract void show();
+
+    @Override
+    public abstract void hide();
+
+}
