f4a0888b35abc570d67ed90e85debddf2f460433
陈斌 <>
2018-11-19 22:44:05 +0800

    杂七杂八页面

---
+++ mainmodule/src/main/java/com/zhihangjia/mainmodule/viewmodel/MessageCenterModuleFragmentVM.java
@@ -0,0 +1,33 @@
+package com.zhihangjia.mainmodule.viewmodel;
+
+import com.nmssdmf.commonlib.callback.BaseRecyclerViewFragmentCB;
+import com.nmssdmf.commonlib.viewmodel.BaseRecyclerViewFragmentVM;
+import com.zhixingjia.bean.mainmodule.IndexBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create by chenbin on 2018/11/19
+ * <p>
+ * <p>
+ */
+public class MessageCenterModuleFragmentVM extends BaseRecyclerViewFragmentVM {
+    /**
+     * 不需要callback可以传null
+     *
+     * @param callBack
+     */
+    public MessageCenterModuleFragmentVM(BaseRecyclerViewFragmentCB callBack) {
+        super(callBack);
+    }
+
+    @Override
+    public void initData(boolean isRefresh) {
+        List<IndexBean.ForumBean> list = new ArrayList<>();
+        for (int i=0; i < 10; i++) {
+            list.add(new IndexBean.ForumBean());
+        }
+        baseCB.refreshAdapter(isRefresh,list);
+    }
+}
