e31285a6e821fb15e78d8168d93f92d409e691aa
陈斌 <>
2018-12-05 22:49:38 +0800

    页面跳转

--- mainmodule/src/main/java/com/zhihangjia/mainmodule/adapter/DailyHotNewsAdapter.java
+++ mainmodule/src/main/java/com/zhihangjia/mainmodule/adapter/DailyHotNewsAdapter.java
@@ -1,16 +1,20 @@
 package com.zhihangjia.mainmodule.adapter;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.nmssdmf.commonlib.bean.Base;
+import com.nmssdmf.commonlib.config.IntentConfig;
 import com.nmssdmf.commonlib.glide.util.GlideUtil;
 import com.nmssdmf.commonlib.util.DensityUtil;
 import com.nmssdmf.customerviewlib.databindingbase.BaseBindingViewHolder;
 import com.nmssdmf.customerviewlib.databindingbase.BaseDataBindingAdapter;
 import com.zhihangjia.mainmodule.R;
+import com.zhihangjia.mainmodule.activity.MessageDetailActivity;
 import com.zhihangjia.mainmodule.databinding.ItemDailyHotNewsBinding;
 import com.zhihangjia.mainmodule.databinding.ItemMessageBinding;
 import com.zhixingjia.bean.mainmodule.IndexBean;
@@ -50,5 +54,16 @@ public class DailyHotNewsAdapter extends BaseDataBindingAdapter<IndexBean.ForumB
                 index++;
             }
         }
+        itemMessageBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                Bundle bundle = new Bundle();
+                intent.setClass(mContext, MessageDetailActivity.class);
+                bundle.putString(IntentConfig.ID, item.getBbs_id());
+                intent.putExtras(bundle);
+                mContext.startActivity(intent);
+            }
+        });
     }
 }
