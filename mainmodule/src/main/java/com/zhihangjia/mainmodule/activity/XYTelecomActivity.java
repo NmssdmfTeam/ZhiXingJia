be7d3844120c20541ba7c3d03b2bb76af7e14b60
chenbin <wscb1030>
2019-01-21 17:16:25 +0800

    宜兴电信页面

--- mainmodule/src/main/java/com/zhihangjia/mainmodule/activity/XYTelecomActivity.java
+++ mainmodule/src/main/java/com/zhihangjia/mainmodule/activity/XYTelecomActivity.java
@@ -1,30 +1,38 @@
 package com.zhihangjia.mainmodule.activity;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.nmssdmf.commonlib.activity.BaseActivity;
+import com.nmssdmf.commonlib.activity.WebViewActivity;
+import com.nmssdmf.commonlib.config.IntentConfig;
+import com.nmssdmf.commonlib.util.DensityUtil;
 import com.nmssdmf.commonlib.util.WindowUtil;
 import com.nmssdmf.commonlib.viewmodel.BaseVM;
-import com.nmssdmf.customerviewlib.OnDataChangeListener;
 import com.zhihangjia.mainmodule.R;
-import com.zhihangjia.mainmodule.adapter.XYTelecomAdapter;
+import com.zhihangjia.mainmodule.adapter.YXTelecomViewPagerAdapter;
 import com.zhihangjia.mainmodule.callback.XYTelecomCB;
 import com.zhihangjia.mainmodule.databinding.ActivityXytelecomBinding;
+import com.zhihangjia.mainmodule.databinding.ItemXyTelecomBinding;
 import com.zhihangjia.mainmodule.viewmodel.XYTelecomVM;
 import com.zhixingjia.bean.mainmodule.YXTelecom;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 宜兴广告
+ * 宜兴广告 宜兴电信营业厅
  */
 public class XYTelecomActivity extends BaseActivity implements XYTelecomCB{
     private final String TAG = XYTelecomActivity.class.getSimpleName();
 
     private XYTelecomVM vm;
     private ActivityXytelecomBinding binding;
-    private XYTelecomAdapter adapter;
+    private YXTelecomViewPagerAdapter rollPageViewAdapter;
 
     @Override
     public String getTAG() {
@@ -44,28 +52,21 @@ public class XYTelecomActivity extends BaseActivity implements XYTelecomCB{
 
     @Override
     protected void initAll(Bundle savedInstanceState) {
-
         WindowUtil.setWindowStatusBarTransParent(this);
-
         binding = (ActivityXytelecomBinding) baseBinding;
+        rollPageViewAdapter = new YXTelecomViewPagerAdapter(binding.rpv);
+        binding.rpv.setAdapter(rollPageViewAdapter);
+        binding.rpv.pause();
         vm.getData(true);
-        adapter = new XYTelecomAdapter(new ArrayList<>());
-
-        binding.crv.setAdapter(adapter);
         binding.ivBack.setOnClickListener(v -> onBackPressed());
         setListener();
     }
 
     private void setListener() {
-        binding.crv.setOnDataChangeListener(new OnDataChangeListener() {
+        binding.srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 vm.getData(true);
-            }
-
-            @Override
-            public void onLoadMore() {
-                vm.getData(false);
             }
         });
     }
@@ -73,14 +74,36 @@ public class XYTelecomActivity extends BaseActivity implements XYTelecomCB{
     @Override
     public void setData(List<YXTelecom> telecoms, boolean isRefresh) {
         if (isRefresh) {
-            binding.crv.setRefreshing(false);
+            binding.llContent.removeAllViews();
         }
-        adapter.notifyDataChangedAfterLoadMore(isRefresh, telecoms);
+        int size = telecoms.size() > 3 ? 3:telecoms.size();
+        for (int i = 0; i < size; i++) {
+            ItemXyTelecomBinding itemXyTelecomBinding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.item_xy_telecom, null, false);
+            int finalI = i;
+            itemXyTelecomBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (TextUtils.isEmpty(telecoms.get(finalI).getLink_url()))
+                        return;
+                    Intent intent = new Intent();
+                    Bundle bundle = new Bundle();
+                    bundle.putString(IntentConfig.LINK, telecoms.get(finalI).getLink_url());
+                    intent.putExtras(bundle);
+                    intent.setClass(XYTelecomActivity.this, WebViewActivity.class);
+                    XYTelecomActivity.this.startActivity(intent);
+                }
+            });
+            itemXyTelecomBinding.setData(telecoms.get(i));
+            binding.llContent.addView(itemXyTelecomBinding.getRoot());
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) itemXyTelecomBinding.getRoot().getLayoutParams();
+            layoutParams.topMargin = DensityUtil.dpToPx(this, 16);
+            itemXyTelecomBinding.getRoot().setLayoutParams(layoutParams);
+        }
+        binding.rpv.pause();
     }
 
     @Override
     public void endFresh() {
-        binding.crv.setRefreshing(false);
-        adapter.notifyDataChangedAfterLoadMore(true);
+        binding.srl.setRefreshing(false);
     }
 }
